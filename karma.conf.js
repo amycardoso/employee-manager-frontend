f4f51c9fa7ea87b50befb730695f1fcc1ba63f9d
Amélia Alice <alice.cardoso10@gmail.com>
2021-05-05 21:32:05 -0300

    :bug: trying to fix build issues

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/employeemanagerapp'),
+      dir: require('path').join(__dirname, './coverage/employee-manager-frontend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/employee-manager-front'));
+app.use(express.static(__dirname + '/dist/employee-manager-frontend'));
 app.get('/*', function(req,res) {
-   res.sendFile(path.join(__dirname + '/dist/employee-manager-front/index.html'));
+   res.sendFile(path.join(__dirname + '/dist/demployee-manager-frontend/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log(`Running on port ${process.env.PORT || 8080}`)
