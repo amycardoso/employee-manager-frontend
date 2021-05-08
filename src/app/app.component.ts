6bc58fc60f307fda7470a1885ebb65c8b166bccf
Amélia Alice <alice.cardoso10@gmail.com>
2021-05-08 14:24:52 -0300

    :construction: adding template and displaying employees

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee-manager-frontend';
+
+export class AppComponent implements OnInit {
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    console.log('Fetching all employees...');
+      this.employeeService.getEmployees().subscribe(
+        (response: Employee[]) => {
+          console.log(response);
+          this.employees = response;
+        },
+        (error: HttpErrorResponse) => {
+          console.error(error);
+        }
+    );
+  }
+
 }
